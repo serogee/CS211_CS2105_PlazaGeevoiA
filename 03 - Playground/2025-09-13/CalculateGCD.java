a1bcc500e1790afba07ce67839c3cd57c20e7141
serosero <sggblank@gmail.com>
2025-09-13 09:50:52 +0800

    🧪 GCD Calculator

---
+++ 03 - Playground/2025-09-13/CalculateGCD.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculateGCD {
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = input.nextInt();
+        System.out.print("Enter b: ");
+        int b = input.nextInt();
+        System.out.println("GCD = " + gcd(a, b));
+        input.close();
+    }
+}
