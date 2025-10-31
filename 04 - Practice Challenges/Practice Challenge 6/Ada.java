60f8f442d95d3a194f98ed23d23e521ff2222556
serosero <sggblank@gmail.com>
2025-10-31 22:21:43 +0800

    🔌 Ada.java WIP

---
+++ 04 - Practice Challenges/Practice Challenge 6/Ada.java
@@ -0,0 +1,14 @@
+// Practice Challenge 6 
+// Ada Lovelace
+import java.util.Scanner;
+
+public class Ada { 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter text: ");
+        input.nextLine();
+
+        input.close();
+     }
+}
